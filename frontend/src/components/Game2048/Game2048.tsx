5c93abc0434eda48e1d8c3c92f1e01e01df8350b
Samuel Meijer <samuel.j.meijer@gmail.com>
2022-02-07 11:42:58 +0100

    Chaning how tiles are generated in Game2048-component

--- frontend/src/components/Game2048/Game2048.tsx
+++ frontend/src/components/Game2048/Game2048.tsx
@@ -4,39 +4,48 @@ import Styles from "./game2048.module.css";
 import Tile from "./Tile/Tile";
 
 // TODO: Add logic for user being logged in or not
-export default function Game2048() { 
-    
-    const activeTileList: {value: number, positionX: number, positionY: number }[] = []
+export default function Game2048() {
+  const tileList: { value: number; positionX: number; positionY: number }[] =
+    [];
 
-    // Generates a new tile with the value 2 on an empty slot.
-    const generateTile = () => {
-        const tile = {
-            value: 2,
-            positionX: Math.floor(Math.random() * 4),
-            positionY: Math.floor(Math.random() * 4),
-        }
+  // Generates a new tile with the value 2 on an empty slot (tile with value = 0).
+  const generateNewValueTile = (): void => {
+    // TODO: Add check to see if all tiles have a value > 0
 
-        // Check that a tile with that position doesn't already exist
-        if (!activeTileList.some((element) => element.positionX === tile.positionX && element.positionY === tile.positionY)) {
-            activeTileList.push(tile)
-        } else generateTile()
+    const randomNumber = Math.floor(Math.random() * tileList.length);
+
+    if (tileList[randomNumber].value === 0) {
+      tileList[randomNumber].value = 2;
+    } else generateNewValueTile();
+  };
+
+  const populateTileList = (): void => {
+    for (let i = 0; i < 4; i++) {
+      tileList.push({ value: 0, positionX: 0, positionY: i });
+      tileList.push({ value: 0, positionX: 1, positionY: i });
+      tileList.push({ value: 0, positionX: 2, positionY: i });
+      tileList.push({ value: 0, positionX: 3, positionY: i });
     }
-    /* Calling to generate two new tiles (game start)
-    generateTile()
-    generateTile()
-    */
+  };
 
-    // TODO: REMOVE! Testing to populate full board
-    for (let i= 0; i < 16; i++) {
-        generateTile()
-    }
+  populateTileList();
 
+  /* Calling to generate two new tiles (game start) */
+  generateNewValueTile();
+  generateNewValueTile();
 
-    return (
-        <div className={Styles.gameGrid}>
-            {activeTileList.map((tile, index) => {
-                return <Tile key={index} value={tile.value} positionX={tile.positionX} positionY={tile.positionY} />
-            })}
-        </div>
-    )
+  return (
+    <div className={Styles.gameGrid}>
+      {tileList.map((tile, index) => {
+        return (
+          <Tile
+            key={index}
+            value={tile.value}
+            positionX={tile.positionX}
+            positionY={tile.positionY}
+          />
+        );
+      })}
+    </div>
+  );
 }
--- frontend/src/components/Game2048/Tile/Tile.tsx
+++ frontend/src/components/Game2048/Tile/Tile.tsx
@@ -3,19 +3,24 @@ import React, { CSSProperties } from 'react';
 import Styles from './tile.module.css'
 
 interface tileProps {
-    // 0, 2, 4 ... 2048
+    // The tiles value: 0, 2, 4 ... 2048
     value: number,
-    // The tiles horistontal position 0-3
+    // The tiles horistontal position: 0-3
     positionX: number,
-    // The tiles vertical position 0-3
+    // The tiles vertical position: 0-3
     positionY: number
 }
 
 export default function Tile ({value, positionX, positionY}: tileProps) {
-
+    
+    const dynamicPositioning = (position:number): number => {
+        return 20 + (120 * position) + (20 * position)
+    }
+    
+    // Dynamic positioning
     const tileStyle: CSSProperties = {
-        top: `${20 + (120 * positionY) + (20 * positionY)}px`,
-        left: `${20 + (120 * positionX) + (20 * positionX)}px`
+        top: `${dynamicPositioning(positionY)}px`,
+        left: `${dynamicPositioning(positionX)}px`
     }
 
     return (
