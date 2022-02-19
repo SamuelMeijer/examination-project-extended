557ebcbd025c6bc4bb86679b5f7c84b96f613230
Samuel Meijer <samuel.j.meijer@gmail.com>
2022-02-20 00:00:07 +0100

    Adding basic functionality to display data from  highscorelist

--- frontend/src/pages/Game/Game.tsx
+++ frontend/src/pages/Game/Game.tsx
@@ -1,9 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Styles from "./game.module.css";
 
 import Game2048 from "../../components/Game2048/Game2048";
 
+// Importing hooks
+import {
+  useAuthenticatedUser
+} from "../../hooks/authenticatedUserHook";
+
+interface highScoreInterface {
+  attributes: {
+    username: string,
+    points: number,
+    moves: number,
+    didWin: boolean,
+  },
+  id: number,
+}
+
 export default function Game() {
+  const authenticatedUser = useAuthenticatedUser();
+
+  const [highscoreList, setHighscoreList] = useState<highScoreInterface[]>([])
+  const [topFiveHighscore, setTopFiveHighscoreList] = useState<highScoreInterface[]>([])
+  const [playerHighscore, setPlayerHighscore] = useState<highScoreInterface>({attributes: {
+    username: '',
+    points: 0,
+    moves: 0,
+    didWin: false,
+  },
+  id: 0,})
+
+  useEffect(() => {
+    // Fetching highscorelist
+    fetch("http://localhost:1337/api/highscores")
+      .then((res) => {
+        if (!res.ok) {
+          throw Error(res.statusText);
+        } else {
+          return res.json();
+        }
+      })
+      .then((data) => {
+        // Check if the authenticated user is included
+        const userHighScore = data.data.find((element: any) => element.attributes.username === authenticatedUser?.user.username)
+
+        if (userHighScore) {
+          setPlayerHighscore(userHighScore)
+        }
+        const topFive = data.data.filter((element: any, index: number) => index < 5)
+        setTopFiveHighscoreList(topFive)
+        setHighscoreList(data.data)
+      })
+      .catch((err) => console.error(err));
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   return (
     <section className={Styles.gameContentContainer}>
       <div className={Styles.instructionsContainer}>
@@ -40,16 +92,25 @@ export default function Game() {
           <h2>Highscore</h2>
         </div>
         <h3 className={Styles.playerHighscoreTitle}>Din bästa omgång</h3>
-        <div className={Styles.playerHighscore}>
-        <div className={Styles.scoreBoardPoints}>
-            <h3>Poäng</h3>
-            <p>0</p>
-          </div>
-          <div className={Styles.scoreBoardPoints}>
-            <h3>Drag</h3>
-            <p>0</p>
-          </div>
-        </div>
+          { authenticatedUser ?
+            <div className={Styles.playerHighscore}>
+              <div className={Styles.scoreBoardPoints}>
+                <h3>Poäng</h3>
+                <p>{playerHighscore.attributes.points ? playerHighscore.attributes.points : 0}</p>
+              </div>
+              <div className={Styles.scoreBoardPoints}>
+                <h3>Drag</h3>
+                <p>{playerHighscore.attributes.moves ? playerHighscore.attributes.moves : 0}</p>
+              </div>
+            </div>
+          :
+            <div className={Styles.playerHighscore}>
+              <div className={Styles.scoreBoardPoints}>
+                <p>Du är inte inloggad</p>
+              </div>
+            </div>
+          }
+      </div>
 
         <div className={Styles.highscoreListContainer}>
             <table className={Styles.highscoreList}>
@@ -61,36 +122,19 @@ export default function Game() {
                 </tr>
               </thead>
               <tbody>
-                {/* TODO: ADD DATA FROM DB */}
-                <tr>
-                  <td>PLAYER A</td>
-                  <td>1337</td>
-                  <td>26</td>
-                </tr>
-                <tr>
-                  <td>PLAYER A</td>
-                  <td>1337</td>
-                  <td>26</td>
-                </tr>
-                <tr>
-                  <td>PLAYER A</td>
-                  <td>1337</td>
-                  <td>26</td>
-                </tr>
-                <tr>
-                  <td>PLAYER A</td>
-                  <td>1337</td>
-                  <td>26</td>
-                </tr>
-                <tr>
-                  <td>PLAYER A</td>
-                  <td>1337</td>
-                  <td>26</td>
-                </tr>
+                {/* Adding data top5 data from highscore */}
+                {topFiveHighscore.map((element, index) => {
+                  return (
+                    <tr key={index}>
+                      <td>{index + 1}. {element.attributes.username}</td>
+                      <td>{element.attributes.points}</td>
+                      <td>{element.attributes.moves}</td>
+                    </tr>
+                  )
+                })}
               </tbody>
             </table>
         </div>
-      </div>
     </section>
   );
 }
