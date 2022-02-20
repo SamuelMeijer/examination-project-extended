a78690c1d7505153141f4e5e02faaecc4c824b00
Samuel Meijer <samuel.j.meijer@gmail.com>
2022-02-20 21:35:45 +0100

    Adding functionality for ordering a shirt from the shop

--- frontend/src/pages/Shop/Shop.tsx
+++ frontend/src/pages/Shop/Shop.tsx
@@ -1,9 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Styles from "./shop.module.css";
 
-import { useAuthenticatedUser, useUpdateAuthenticatedUser} from "../../hooks/authenticatedUserHook";
-
-import StyledButton from "../../components/StyledButton/StyledButton";
+import { useAuthenticatedUser } from "../../hooks/authenticatedUserHook";
+import { useAuthenticatedUserHighscore} from "../../hooks/authenticatedUserHighscoreHook";
 
 interface shopItemInterface {
   id: number;
@@ -16,12 +15,75 @@ interface shopItemInterface {
   };
 }
 
+interface userOrdersInterface {
+  id: number;
+  attributes: {
+    username: string;
+    shopitems: {
+      data: shopItemInterface[]
+      }
+    }
+}
+
 export default function Shop() {
-  const [shopItems, setShopItems] = useState<shopItemInterface[]>([]);
   const authenticatedUser = useAuthenticatedUser()
-  const updateAuthenticatedUser = useUpdateAuthenticatedUser()
+  const authenticatedUserHighscore = useAuthenticatedUserHighscore()
+
+  const [shopItems, setShopItems] = useState<shopItemInterface[]>([]);
+  const [userOrders, _setUserOrders] = useState<userOrdersInterface[]>([]);
+  const [purchaseMessage, setPurchaseMessage] = useState<string>('')
+  
+  const userOrdersRef = useRef(userOrders);
+  const setUserOrders = (data: userOrdersInterface[]) => {
+    userOrdersRef.current = data;
+    _setUserOrders(data);
+  }
+
+
+  const handleOnClick = (event: React.MouseEvent) => {
+    event.preventDefault()
+    
+    if (!authenticatedUser) {
+      setPurchaseMessage('Du måste vara inloggad för att kunna beställa')
+    } else if (authenticatedUser && !authenticatedUserHighscore?.attributes.didWin) {
+      setPurchaseMessage('Du måste ha fått 2048 för att kunna beställa')
+    } else if (authenticatedUser && authenticatedUserHighscore?.attributes.didWin && userOrdersRef.current.length > 0) {
+      setPurchaseMessage('Du har redan beställt en tröja')
+    } else {
+      // Placing order
+      const reqBody = {
+        username: authenticatedUser.user.username,
+        // ID of shopitem
+        shopItems: [1]
+      }
+      
+      fetch("http://localhost:1337/api/orders", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${authenticatedUser?.jwt}`,
+        },
+        body: JSON.stringify({data: reqBody})
+      })
+        .then((res) => {
+          if (!res.ok) {
+            throw Error(res.statusText);
+          } else {
+            return res.json();
+          }
+        })
+        .then((data) => {
+          setPurchaseMessage('Din order har accepterats')
+        })
+        .catch((err) => {
+          setPurchaseMessage('Något gick tyvärr fel')
+          console.error(err)
+        });
+    }
+  }
 
   useEffect(() => {
+    // Fetching shopitems
     fetch("http://localhost:1337/api/shopitems")
       .then((res) => {
         if (!res.ok) {
@@ -34,27 +96,50 @@ export default function Shop() {
         setShopItems(data.data)
       })
       .catch((err) => console.error(err));
-  }, []);
 
-  console.log('authenticatedUser: ', authenticatedUser )
-  console.log('updateAuthenticatedUser: ', updateAuthenticatedUser  )
+    if (authenticatedUser) {
+      // Fetching orders
+      fetch("http://localhost:1337/api/orders?populate=*", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${authenticatedUser?.jwt}`,
+        }
+      })
+        .then((res) => {
+          if (!res.ok) {
+            throw Error(res.statusText);
+          } else {
+            return res.json();
+          }
+        })
+        .then((data) => {
+          // Check if the user has made any purchases
+          const userMadeOrders = data.data.filter((element: any) => element.attributes.username === authenticatedUser?.user.username)
+
+          setUserOrders(userMadeOrders)
+        })
+        .catch((err) => console.error(err));
+    }
+    
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <section className={Styles.contentContainer}>
       {shopItems.length > 0 ? (
-        shopItems.map((element, index) => (
-          <section key={element.id} className={index % 2 !== 0 ? `${Styles.contentBoxContainer} ${Styles.midContent}` : `${Styles.contentBoxContainer}`}>
+          <section key={shopItems[0].id} className={Styles.contentBoxContainer}>
             <div
               className={`${Styles.innerContentContainer} ${Styles.innerLeft}`}
             >
               <div className={Styles.colorDivider}>
-                <h2>{element.attributes.title}</h2>
+                <h2>{shopItems[0].attributes.title}</h2>
               </div>
               <p>
-                {element.attributes.shortDescription}
+                {shopItems[0].attributes.shortDescription}
               </p>
               <p>
-                {element.attributes.longDescription}
+                {shopItems[0].attributes.longDescription}
               </p>
             </div>
 
@@ -65,16 +150,40 @@ export default function Shop() {
               <p>
                 PLACE IMAGE HERE
               </p>
-              
-              <StyledButton textInput="Beställ" colorInput="#F78632" />
+              {purchaseMessage? <h3>{purchaseMessage}</h3> : null}
+              <button onClick={handleOnClick}>
+                Beställ
+              </button>
               <div className={Styles.colorDivider} />
             </div>
           </section>
-        ))
       ) : (
-        // TODO: Replace with Spinner
-        <p>Missing content</p>
+        null
       )}
+
+      <section className={`${Styles.contentBoxContainer} ${Styles.midContent}`} >
+            <div
+              className={`${Styles.innerContentContainer} ${Styles.innerLeft}`}
+            >
+              <div className={Styles.colorDivider}>
+                <h2>Framtiden</h2>
+              </div>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, necessitatibus.
+              </p>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, necessitatibus.
+              </p>
+            </div>
+
+            <div
+              className={`${Styles.innerContentContainer} ${Styles.innerRight}`}
+            >
+              <p>Lorem ipsum dolor sit amet.</p> 
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis eligendi excepturi ea aut minus blanditiis saepe numquam vel neque accusantium.</p>
+              <div className={Styles.colorDivider} />
+            </div>
+      </section>
     </section>
   );
 }
