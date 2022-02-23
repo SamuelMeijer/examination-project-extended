542b1924a17404553602ad26ea236121ed103b07
Samuel Meijer <samuel.j.meijer@gmail.com>
2022-02-23 08:21:53 +0100

    Adding NoSuchPage

--- frontend/src/pages/NoSuchPage/NoSuchPage.tsx
+++ frontend/src/pages/NoSuchPage/NoSuchPage.tsx
@@ -1,6 +1,24 @@
 import React from "react";
-import Styles from "noSuchPage.module.css";
+import Styles from "./noSuchPage.module.css";
 
 export default function NoSuchPage() {
-  return <p>This is the page for NoSuchPage</p>;
+  return (
+    <section className={Styles.contentContainer}>
+      <section className={`${Styles.contentBoxContainer} ${Styles.midContent}`} >
+            <div
+              className={`${Styles.innerContentContainer} ${Styles.innerLeft}`}
+            >
+              <div className={Styles.colorDivider}>
+                <h2>Sorry, error...</h2>
+              </div>
+              <p>
+              No such page exists
+              </p>
+              <p>
+              Are you sure you have entered the correct URL?
+              </p>
+            </div>
+      </section>
+    </section>
+    );
 }
